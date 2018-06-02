1fa04796c094de924af030f02860f0ee76bf2330
srappaport <sarah.rappaport@greendot.org>
2018-06-02 23:34:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name]
+    "#{name.reverse!}"
+  end 
+
+  get 'square/:number' do
+    
+  end
+
 
 end
