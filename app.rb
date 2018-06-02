da14edd7fa4833dfd32611ce1b09f831f5534bf0
srappaport <sarah.rappaport@greendot.org>
2018-06-02 23:42:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,7 +15,10 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @phrase = params[:phrase]
     @number=params[:number].to_i
-
+    count=0
+    while count < @number
+      @phrase
+    end
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
