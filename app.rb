723da07c7567efe8696ba7aebfd7418f8601ee8c
srappaport <sarah.rappaport@greendot.org>
2018-06-02 23:52:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,7 +19,12 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-
+    @w1 = params[:word1]
+    @w2 = params[:word2]
+    @w3 = params[:word3]
+    @w4 = params[:word4]
+    @w5 = params[:word5]
+    @w1 @w2 @w3 @w4 @w5
   end
 
   get '/:operation/:number1/:number2' do
