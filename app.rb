26e7032f79fb1a1b6b09c64a79480dae07e0ffc6
srappaport <sarah.rappaport@greendot.org>
2018-06-02 23:45:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
     @phrase = params[:phrase]
-    @number=params[:number].to_i
+    @number=params[:number]
     count=0
     "hello"
     while count < 10
