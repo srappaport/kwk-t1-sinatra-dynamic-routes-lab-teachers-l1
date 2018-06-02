2b53cff0c17ea547a8c68c0c62da3bc4fa1e0d24
srappaport <sarah.rappaport@greendot.org>
2018-06-02 23:50:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,9 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    phrase*number.to_i
+    @phrase = params[:phrase]
+    @number = params[:number]
+    @phrase*@number.to_i
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
